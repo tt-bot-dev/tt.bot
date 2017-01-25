dbeb7636c6d501c888085ed4e7bdc3fe818eb734
TTtie <tttiett@outlook.com>
2017-01-25 15:30:12 +0100

    getAllUsers set to true

--- discord.js
+++ discord.js
@@ -2,7 +2,9 @@ module.exports = function () {
     global.fs = require("fs");
     global.ErisO = require("eris");
     global.Eris = require("./libutil")
-    const bot = new Eris(config.token);
+    const bot = new Eris(config.token,{
+        getAllUsers:true
+    });
     global.isO = function(msg) {
         return msg.author.id == config.oid
     }
