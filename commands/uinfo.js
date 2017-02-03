3101ecb939bf48f058de026d6ed8e71ef2394d77
TTtie <tttiett@outlook.com>
2017-02-03 12:04:13 +0100

    uinfo additions

--- commands/uinfo.js
+++ commands/uinfo.js
@@ -2,21 +2,26 @@ module.exports = {
     exec: function (msg, args) {
         let u = args != "" ? args : msg.author.id;
         userQuery(u, msg).then(u => {
+            let rarr = u.roles;
+            let rnames = [`<@&${msg.guild.id}>`];
+            rarr.forEach(fe => {
+                rnames.push(u.guild.roles.get(fe).name)
+            })
             let s = u.status;
             function getstatus() {
                 switch (s) {
-                case "online":
-                    return "Online";
-                    break;
-                case "idle":
-                    return "Idle";
-                    break;
-                case "dnd":
-                    return "Do not disturb"
-                    break;
-                case "offline":
-                    return "Invisible/offline"
-                    break;
+                    case "online":
+                        return "Online";
+                        break;
+                    case "idle":
+                        return "Idle";
+                        break;
+                    case "dnd":
+                        return "Do not disturb"
+                        break;
+                    case "offline":
+                        return "Invisible/offline"
+                        break;
                 }
             }
             bot.createMessage(msg.channel.id, {
@@ -35,6 +40,18 @@ module.exports = {
                     }, {
                         name: "Status",
                         value: getstatus(),
+                        inline: true
+                    }, {
+                        name: "Roles",
+                        value: rnames.join(", ").length > 2048 ? "Too long to show ;-;" : rnames.join(", "),
+                        inline: true
+                    }, {
+                        name: "Created at",
+                        value: new Date(u.createdAt).toString(),
+                        inline: true
+                    }, {
+                        name: "Is in a voice channel?",
+                        value: u.voiceState.channelID ? "yes" : "no",
                         inline: true
                     }],
                     timestamp: new Date(u.joinedAt)
