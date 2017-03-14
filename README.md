a22406e8a78f7c36a064511d25ccd17ae39aebcf
TTtie <tttiett@outlook.com>
2017-03-14 15:54:36 +0100

    Fixing my markdown and updates to phone emitter

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-# tt.alpha
-[TTtie Bot](https://github.com/TTtie/TTtie-Bot), but with better code.<br>
-It is actually made in [Eris](https://abal.moe/Eris).
+# tt.bot
+tt.bot is a bot with aim for moderation and fun, made in [Eris](https://github.com/abalabahaha/eris).
 
-#Database
+# Features
+<span style="color:red">The bot is being rewritten, so it lacks features it had before.</span>
+- Cross-server telephony
+- Getting user info and their avatar
+
+# Database
 Actually the database does nothing, however, in later future, it might do something.<br>
-The database is RethinkDB, and is a dependency for [`db.js`](./db.js) file
+The database is RethinkDB.
 
-#How to set up
+# How to set up
 1. Rename `exampleconfig.json` to `config.json`
 2. Edit token, oid, prefix and optional, but if you need, dbotskey properties.
 3. Run `run.js`
 
-#Creating events/commands
+# Creating events/commands
 Refer to [EVENTS.md](./EVENTS.md) or [COMMANDS.md](./COMMANDS.md).
 
-#Development
+# Development
 As the name says, the bot is still in development. You can help recreating all [TTtie Bot](https://github.com/TTtie/TTtie-Bot) commands by submitting a pull request.
--- speakerphone/emitter.js
+++ speakerphone/emitter.js
@@ -1,12 +1,16 @@
 let EventEmitter = require("events").EventEmitter
 module.exports = class Emitter extends EventEmitter {
     constructor(channel1, channel2) {
-
         super();
+        this._phoneEmoji = "\u260e"
         this._chan1 = channel1
         this._chan2 = channel2
         let chan1id = channel1.id;
         let chan2id = channel2.id
+        let _phoneEmoji = "\u260e"
+        function getSpeakerPhoneMessage(author, text) {
+            return `${_phoneEmoji} ${author.username}#${author.discriminator}: ${text}`
+        }
         channel1.createMessage(this.getMessage(channel2));
         channel2.createMessage(this.getMessage(channel1));
         const bindingFunction = function (msg) {
@@ -15,11 +19,20 @@ module.exports = class Emitter extends EventEmitter {
                     if (!msg.author) return;
                     if (msg.author.bot) return;
                     else {
-                        if (!msg.content.startsWith("^")) return console.log(`Ain't treating ${msg.content} as a speakerphone message`)
+                        function listAttacments() {
+                            if (msg.attachments.length > 0) {
+                                let urlA = [];
+                                msg.attachments.forEach(item => {
+                                    urlA.push(item.url)
+                                })
+                                return `\n${urlA.join("\n")}`
+                            } else return "";
+                        }
+                        if (!msg.content.startsWith("^")) return;
                         let mToSend = msg.content.slice(1);
-                        if (mToSend) {
-                            if (msg.channel.id == channel1.id) bot.createMessage(channel2.id, `${msg.author.username}#${msg.author.discriminator}: ${mToSend}`);
-                            else if (msg.channel.id == channel2.id) bot.createMessage(channel1.id, `${msg.author.username}#${msg.author.discriminator}: ${mToSend}`);
+                        if (mToSend || listAttacments()) {
+                            if (msg.channel.id == channel1.id) bot.createMessage(channel2.id, getSpeakerPhoneMessage(msg.author, `${mToSend}${listAttacments()}`));
+                            else if (msg.channel.id == channel2.id) bot.createMessage(channel1.id, getSpeakerPhoneMessage(msg.author, `${mToSend}${listAttacments()}`));
                         }
                     }
                 }
@@ -29,11 +42,12 @@ module.exports = class Emitter extends EventEmitter {
         bot.on("messageCreate", bindingFunction)
         this.once("EndSpeakerphone", (endChannel) => {
             bot.removeListener("messageCreate", bindingFunction);
-            if (endChannel == channel1) { channel2.createMessage("The other side has ended the communication."); endChannel.createMessage("You ended the communication.") }
-            else if (endChannel == channel2) { channel1.createMessage("The other side has ended the communication."); endChannel.createMessage("You ended the communication.") }
+            if (endChannel == channel1) { channel2.createMessage(_phoneEmoji + " The other side has ended the communication."); endChannel.createMessage(_phoneEmoji + " You ended the communication.") }
+            else if (endChannel == channel2) { channel1.createMessage(_phoneEmoji + " The other side has ended the communication."); endChannel.createMessage(_phoneEmoji + " You ended the communication.") }
         })
     }
     getMessage(channel) {
-        return `Connected to #${channel.name} at ${channel.guild.name}\n\nPrefix your messages with \`^\` to send them over.`
+        return `${this._phoneEmoji} Connected to #${channel.name} at ${channel.guild.name}\n\nPrefix your messages with \`^\` to send them over.`
     }
+
 }
