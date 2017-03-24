02c5938d30bb03f4777ea16f3e3654c8bac274b4
TTtie <tttiett@outlook.com>
2017-03-24 10:18:12 +0100

    more changes to command management command

--- commands/manage.js
+++ commands/manage.js
@@ -3,46 +3,50 @@ module.exports = {
         if (isO(msg)) {
             let c = args.split(" ");
             let action = c[0];
-            switch (action) {
-                case "unload":
-                    return bot.createMessage(msg.channel.id, `Unloading command ${c[1]}`).then(m => {
-                        try {
-                            cmdWrap.unload(c[1])
-                            return m.edit(`Unloaded the command ${c[1]}`)
-                        } catch (err) {
-                            return m.edit(`Cannot unload the command ${c[1]} ${err}`)
-                        }
-                    })
-                    break;
-                case "load":
-                    return bot.createMessage(msg.channel.id, `Loading command ${c[1]}`).then(m => {
-                        try {
-                            if (c[1] == "all") cmdWrap.loadAll(); else cmdWrap.load(c[1])
-                            return m.edit(`Loaded the command ${c[1]}`)
-                        } catch (err) {
-                            return m.edit(`Cannot load the command ${c[1]} ${err}`)
-                        }
-                    })
-                    break;
-                case "reload":
-                    return bot.createMessage(msg.channel.id, `Reloading command ${c[1]}`).then(m => {
-                        try {
-                            cmdWrap.reload(c[1])
-                            return m.edit(`Reloaded the command ${c[1]}`)
-                        } catch (err) {
-                            return m.edit(`Cannot reload the command ${c[1]} ${err}`)
-                        }
-                    })
-                    break;
-                default:
-                    return bot.createMessage(msg.channel.id, "You haven't chosen a valid command management command.")
-                    break;
+            if (action && c[1]) {
+                switch (action) {
+                    case "unload":
+                        return bot.createMessage(msg.channel.id, `Unloading command ${c[1]}`).then(m => {
+                            try {
+                                cmdWrap.unload(c[1])
+                                return m.edit(`Unloaded the command ${c[1]}`)
+                            } catch (err) {
+                                return m.edit(`Cannot unload the command ${c[1]} ${err}`)
+                            }
+                        })
+                        break;
+                    case "load":
+                        return bot.createMessage(msg.channel.id, `Loading command ${c[1]}`).then(m => {
+                            try {
+                                if (c[1] == "all") cmdWrap.loadAll(); else cmdWrap.load(c[1])
+                                return m.edit(`Loaded the command ${c[1]}`)
+                            } catch (err) {
+                                return m.edit(`Cannot load the command ${c[1]} ${err}`)
+                            }
+                        })
+                        break;
+                    case "reload":
+                        return bot.createMessage(msg.channel.id, `Reloading command ${c[1]}`).then(m => {
+                            try {
+                                cmdWrap.reload(c[1])
+                                return m.edit(`Reloaded the command ${c[1]}`)
+                            } catch (err) {
+                                return m.edit(`Cannot reload the command ${c[1]} ${err}`)
+                            }
+                        })
+                        break;
+                    default:
+                        return bot.createMessage(msg.channel.id, "You haven't chosen a valid command management command.")
+                        break;
+                }
+            } else {
+                return bot.createMessage(msg.channel.id, "You haven't chosen a valid command management command or you are missing required arguments.")
             }
         }
     },
     isCmd: true,
     display: true,
     category: 2,
-    description: "Say something.",
-    args: "[]"
+    description: "Command management",
+    args: "<reload/load [all]/unload> <command name>"
 }
