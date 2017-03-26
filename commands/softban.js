7997a78f53334b6588099fde0d3412a9b8130930
TTtie <tttiett@outlook.com>
2017-03-26 16:36:13 +0200

    softban command

---
+++ commands/softban.js
@@ -0,0 +1,24 @@
+module.exports = {
+    exec: async function (msg, args) {
+        if (args) {
+            if (await bot.isModerator(msg.member)) {
+                try {
+                    let user = await userQuery(args, msg)
+                    await user.ban(1);
+                    await user.unban()
+                    await msg.channel.createMessage(`:ok_hand: Softbanned ${user.username}#${user.discriminator}.`)
+                } catch(err) {
+                    bot.createMessage(msg.channel.id, "```xl\nError:\n" + err + "\n```").then(null, console.error)
+                    console.error(err)
+                }
+            }
+        } else {
+            return await bot.createMessage(msg.channel.id, `**${msg.author.username}**, you miss required arguments.`)
+        }
+    },
+    isCmd: true,
+    display: true,
+    category: 3,
+    description: "Softbans a user.",
+    args: "<user>"
+}
