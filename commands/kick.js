2f8a0387fdc0217292e0ea6f01b22e63f680a9ac
TTtie <tttiett@outlook.com>
2017-03-26 16:34:30 +0200

    kick command

---
+++ commands/kick.js
@@ -0,0 +1,23 @@
+module.exports = {
+    exec: async function (msg, args) {
+        if (args) {
+            if (await bot.isModerator(msg.member)) {
+                try {
+                    let user = await userQuery(args, msg)
+                    await user.kick();
+                    await msg.channel.createMessage(`:ok_hand: Kicked ${user.username}#${user.discriminator}.`)
+                } catch(err) {
+                    bot.createMessage(msg.channel.id, "```xl\nError:\n" + err + "\n```").then(null, console.error)
+                    console.error(err)
+                }
+            }
+        } else {
+            return await bot.createMessage(msg.channel.id, `**${msg.author.username}**, you miss required arguments.`)
+        }
+    },
+    isCmd: true,
+    display: true,
+    category: 3,
+    description: "Kicks a user.",
+    args: "<user>"
+}
