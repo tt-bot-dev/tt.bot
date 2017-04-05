bac76122cb7b4c64626b46ea0de126586213e430
TTtie <tttiett@outlook.com>
2017-04-05 17:16:21 +0200

    idk how to describe

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 module.exports = {
     exec: function (msg, args) {
         let things = args.split(" ")
-        bot.createMessage(msg.channel.id, `Here you go! <https://discordapp.com/oauth2/authorize?client_id=264323176329117697&scope=bot${isNaN(parseInt(things[0])) == false ? `&permissions=${things[0]}` : ""}${isNaN(parseInt(things[1])) == false ? `&guild_id=${things[1]}` : ""}>\n\nIf you need support using the bot, come to our support server https://discord.gg/pGN5dMq`)
+        bot.createMessage(msg.channel.id, `Here you go! <https://discordapp.com/oauth2/authorize?client_id=264323176329117697&scope=bot${isNaN(parseInt(things[0])) == false ? `&permissions=${things[0]}` : ""}${isNaN(parseInt(things[1])) == false ? `&guild_id=${things[1]}` : ""}>\n\nIf you need help using the bot, come to our support server, invite is in info command.`)
     },
     name: "invite",
     isCmd: true,
