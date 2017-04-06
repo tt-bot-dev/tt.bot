cf2f9718c1dac5a2e429a971a0250902fab71b2e
TTtie <tttiett@outlook.com>
2017-04-06 15:23:02 +0200

    more edits

--- commands/getprefix.js
+++ commands/getprefix.js
@@ -1,9 +1,9 @@
 module.exports = {
     exec: async function (msg, args) {
         if (msg.guildConfig && msg.guildConfig.prefix !== config.prefix) {
-            bot.createMessage(msg.channel.id, `The actual prefix for this server is \`${msg.guildConfig.prefix}\`.\nYou can still use \`tt.\` prefix. `)
+            bot.createMessage(msg.channel.id, `The actual prefix for this server is \`${msg.guildConfig.prefix}\`.\nYou can still use \`${config.prefix}\` prefix. `)
         } else {
-            bot.createMessage(msg.channel.id, "This server hasn't set any prefix. You can use `tt.` prefix.")
+            bot.createMessage(msg.channel.id, `This server hasn't set any prefix. You can use \`${config.prefix}\` prefix.`)
         }
     },
     isCmd: true,
