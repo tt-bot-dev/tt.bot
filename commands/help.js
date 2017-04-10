d17818ab0ac13973fde6c7c65e20d439875896b3
TTtie <tttiett@outlook.com>
2017-04-10 17:54:23 +0200

    clear description edits and embed help

--- commands/help.js
+++ commands/help.js
@@ -17,22 +17,60 @@ module.exports = {
             Object.keys(cmds).forEach(doShit)
             let tosnd = [];
             if (gencmds.length > 0) {
-                tosnd.push(`General commands:\n${gencmds.join(", ")}\n`)
+                tosnd.push({
+                    name: "General commands",
+                    value: gencmds.join(", ")
+                })
             }
             if (await bot.isModerator(msg.member) && modcmds.length > 0) {
-                tosnd.push(`Moderator commands:\n${modcmds.join(", ")}\n`)
+                tosnd.push({
+                    name: "Moderator commands",
+                    value: modcmds.join(", ")
+                })
             }
             if (isO(msg) && ocmds.length > 0) {
-                tosnd.push(`Owner commands:\n${ocmds.join(", ")}`)
+                tosnd.push({
+                    name: "Owner commands",
+                    value: ocmds.join(", ")
+                })
             }
 
             bot.getDMChannel(msg.author.id).then(dm => {
-                return bot.createMessage(dm.id, `\`\`\`prolog\n${tosnd.join("\n") || "No commands, I guess."}\`\`\`\nYou can also do \`${config.prefix}help (command)\` to get additional command help.`)
+                msg.channel.createMessage(msg.author.mention + " Sent you a PM with help.")
+                return bot.createMessage(dm.id, {
+                    embed: {
+                        author: {
+                            name: "Help"
+                        },
+                        description: "Here, have a list of commands.",
+                        fields: tosnd.length > 0 ? tosnd : [{
+                            name: "There are no commands.",
+                            value: "Sorry for that."
+                        }],
+                        color: 0x008800,
+                        footer: {
+                            text: `You can do ${config.prefix}help <command> to get additional command help. | Powered by tt.bot`
+                        }
+                    }
+                })
             })
         } else {
             bot.getDMChannel(msg.author.id).then(dm => {
                 if (cmds[args]) {
-                    bot.createMessage(dm.id, `Help for **${cmds[args].name}** command:\nArguments: \`${cmds[args].args != null ? cmds[args].args : "None"}\`\nDescription: ${cmds[args].description != null ? cmds[args].description : "None"}`)
+                    msg.channel.createMessage(`${msg.author.mention} Sent you a PM with help for ${args} command`)
+                    bot.createMessage(dm.id, {
+                        embed: {
+                            author: {name:`Help for ${args}`},
+                            fields: [{
+                                name: "Arguments",
+                                value: cmds[args].args || "None"
+                            }, {
+                                name: "Description",
+                                value: cmds[args].description || "None"
+                            }],
+                            color: 0x008800
+                        }
+                    })
                 }
             })
         }
