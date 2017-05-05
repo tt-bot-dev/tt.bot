d72e508050199e645e518fe5aa880c952aa482bb
TTtie <tttiett@outlook.com>
2017-05-05 17:45:31 +0200

    make guilds a route

--- webserver/index.js
+++ webserver/index.js
@@ -60,90 +60,7 @@ app.get('/login', checkAuthNeg, passport.authenticate('discord', { scope: scopes
 app.get('/callback',
     passport.authenticate('discord', { failureRedirect: '/' }), function (req, res) { res.redirect('/') } // auth success
 );
-app.get("/guilds/botcolls", checkOwner, (req, res) => {
-    res.render("botcoll", {
-        user: req.isAuthenticated() ? {
-            username: req.user.username,
-            discriminator: req.user.discriminator,
-            avatar: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
-            id: req.user.id
-        } : null
-    })
-})
-app.get("/guilds/botcolls/prune", checkOwner, (req,res) => {
-    if (req.query.yes == "true") {
-        if (bot.listBotColls().length > 0) {
-            bot.listBotColls().forEach(g => g.leave())
-            res.redirect("/guilds/botcolls")
-        }
-    } else {
-        res.render("delete", {
-            guild: {
-                name: "All bot collection servers"
-            }, user: req.isAuthenticated() ? {
-                username: req.user.username,
-                discriminator: req.user.discriminator,
-                avatar: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
-                id: req.user.id
-            } : null
-        })
-    }
-})
-app.get("/guilds/delete/:id", checkOwner, (req, res) => {
-    var guild = bot.guilds.get(req.params.id);
-    if (req.query.yes == "true") {
-        if (guild) {
-            guild.leave()
-            res.redirect("/")
-        } else {
-            res.status(404);
-            res.send("invalid guild")
-        }
-    } else {
-        res.render("delete", {
-            guild: guild, user: req.isAuthenticated() ? {
-                username: req.user.username,
-                discriminator: req.user.discriminator,
-                avatar: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
-                id: req.user.id
-            } : null
-        })
-    }
-})
-app.get("/guilds/invite/:id", checkOwner, async (req, res) => {
-    let guild = bot.guilds.get(req.params.id);
-    if (guild) {
-        let invite;
-        try {
-            invite = await guild.defaultChannel.createInvite()
-        } catch (err) {
-            invite = { error: err };
-        }
-        res.send(invite.error ? `Error while creating an invite: ${err}` : `The invite code is ${invite.code}`)
-    } else return res.end("Invalid guild")
-})
-app.get("/guilds/:id", checkOwner, (req, res) => {
-    let guild = bot.guilds.get(req.params.id);
-    if (guild) return res.render("guild", {
-        guild: guild, user: req.isAuthenticated() ? {
-            username: req.user.username,
-            discriminator: req.user.discriminator,
-            avatar: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
-            id: req.user.id
-        } : null
-    }); else return res.end("Invalid guild")
-})
-app.get("/guilds/:id/members", checkOwner, (req, res) => {
-    let guild = bot.guilds.get(req.params.id);
-    if (guild) return res.render("guild-members", {
-        guild: guild, user: req.isAuthenticated() ? {
-            username: req.user.username,
-            discriminator: req.user.discriminator,
-            avatar: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
-            id: req.user.id
-        } : null
-    }); else return res.end("Invalid guild")
-})
+
 app.get('/logout', checkAuth, function (req, res) {
     req.logout();
     res.redirect('/');
@@ -178,7 +95,7 @@ app.get("/", (req, res) => {
         guilds: bot.guilds.filter(fn => true)
     })
 })
-
+app.use("/guilds", require("./routes/guild"));
 app.use((req,res) => {
     res.status(404).render("404", {
                 user: req.isAuthenticated() ? {
---
+++ webserver/routes/guild.js
@@ -0,0 +1,106 @@
+const Router = require("express").Router;
+const app = Router();
+
+app.get("/botcolls", checkOwner, (req, res) => {
+    res.render("botcoll", {
+        user: req.isAuthenticated() ? {
+            username: req.user.username,
+            discriminator: req.user.discriminator,
+            avatar: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
+            id: req.user.id
+        } : null
+    })
+})
+app.get("/botcolls/prune", checkOwner, (req,res) => {
+    if (req.query.yes == "true") {
+        if (bot.listBotColls().length > 0) {
+            bot.listBotColls().forEach(g => g.leave())
+            res.redirect("/guilds/botcolls")
+        }
+    } else {
+        res.render("delete", {
+            guild: {
+                name: "All bot collection servers"
+            }, user: req.isAuthenticated() ? {
+                username: req.user.username,
+                discriminator: req.user.discriminator,
+                avatar: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
+                id: req.user.id
+            } : null
+        })
+    }
+})
+app.get("/delete/:id", checkOwner, (req, res) => {
+    var guild = bot.guilds.get(req.params.id);
+    if (req.query.yes == "true") {
+        if (guild) {
+            guild.leave()
+            res.redirect("/")
+        } else {
+            res.status(404);
+            res.send("invalid guild")
+        }
+    } else {
+        res.render("delete", {
+            guild: guild, user: req.isAuthenticated() ? {
+                username: req.user.username,
+                discriminator: req.user.discriminator,
+                avatar: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
+                id: req.user.id
+            } : null
+        })
+    }
+})
+app.get("/invite/:id", checkOwner, async (req, res) => {
+    let guild = bot.guilds.get(req.params.id);
+    if (guild) {
+        let invite;
+        try {
+            invite = await guild.defaultChannel.createInvite()
+        } catch (err) {
+            invite = { error: err };
+        }
+        res.send(invite.error ? `Error while creating an invite: ${err}` : `The invite code is ${invite.code}`)
+    } else return res.end("Invalid guild")
+})
+app.get("/:id", checkOwner, (req, res) => {
+    let guild = bot.guilds.get(req.params.id);
+    if (guild) return res.render("guild", {
+        guild: guild, user: req.isAuthenticated() ? {
+            username: req.user.username,
+            discriminator: req.user.discriminator,
+            avatar: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
+            id: req.user.id
+        } : null
+    }); else return res.end("Invalid guild")
+})
+app.get("/:id/members", checkOwner, (req, res) => {
+    let guild = bot.guilds.get(req.params.id);
+    if (guild) return res.render("guild-members", {
+        guild: guild, user: req.isAuthenticated() ? {
+            username: req.user.username,
+            discriminator: req.user.discriminator,
+            avatar: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
+            id: req.user.id
+        } : null
+    }); else return res.end("Invalid guild")
+})
+
+function checkOwner(req, res, next) {
+    if (req.isAuthenticated()) {
+        if (req.user.id == config.oid) return next()
+    }
+    res.send("You're not owner.")
+}
+
+function checkAuth(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
+}
+
+function checkAuthNeg(req, res, next) {
+    if (!req.isAuthenticated()) return next();
+    res.send('you\'re logged in already:)');
+}
+
+module.exports = app;
