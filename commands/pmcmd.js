359b3f735c79d69390b012070353ea1ebcabfbff
TTtie <tttiett@outlook.com>
2017-05-07 12:27:50 +0200

    a command to PM command output

---
+++ commands/pmcmd.js
@@ -0,0 +1,27 @@
+module.exports = {
+    exec: async function (msg, args) {
+            if (args) {
+                let c = args.split(" ");
+                let cmd = c[0]
+                let cmdargs = c.slice(1).join(" ");
+                let commandToExec = cmds[cmd];
+                if (commandToExec) {
+                    let guild = msg.guild
+                    let member = msg.member
+                    msg.channel = await bot.getDMChannel(msg.author.id)
+                    msg.member = member
+                    msg.channel.guild = guild;
+                    msg.guild = guild;
+                    commandToExec.exec(msg, cmdargs)
+                }
+            }
+            else {
+                msg.channel.createMessage(`What command I should PM you?`)
+            }
+    },
+    isCmd: true,
+    display: true,
+    category: 1,
+    description: "PMs you the command output.",
+    args: "<command> [command args]"
+}
