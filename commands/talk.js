e2be2d1271f727bdf4301f7c08bc0a33010d6306
TTtie <tttiett@outlook.com>
2017-06-04 12:44:37 +0200

    cleverbot fix

--- commands/talk.js
+++ commands/talk.js
@@ -7,12 +7,23 @@ module.exports = {
         }
         if (args > 255) return msg.channel.createMessage(errMessages.queryTooLong)
         try {
-            let r = await s.get(`http://api.program-o.com/v2/chatbot/?bot_id=6&say=${encodeURIComponent(args)}&convo_id=${msg.author.id}&format=json`)
-            .set("Accept", "application/json")
-            .set("User-Agent", "tt.bot (https://github.com/tttie/tttie-bot)")
-            msg.channel.createMessage(r.body.botsay.replace(/Program-O/g, bot.user.username).replace(/\<br\/\>/g, "\n"))
-        } catch(err) {
+            let r = await s.get(`http://api.program-o.com/v2/chatbot/`)
+                .query({
+                    //?bot_id=6&say=${encodeURIComponent(args)}&convo_id=${msg.author.id}&format=json
+                    bot_id: 6,
+                    say: encodeURIComponent(args),
+                    convo_id: msg.author.id,
+                    format: "json"
+                })
+               /* .set("Accept", "application/json")
+                .set("User-Agent", "tt.bot (https://github.com/tttie/tttie-bot)")*/
+            r.body = JSON.parse(r.text)
+            let resp = r.body.botsay.replace(/Program-O/g, bot.user.username).replace(/\<br\/\>/g, "\n")
+            
+            await msg.channel.createMessage(resp)
+        } catch (err) {
             msg.channel.createMessage(errMessages.cantTell)
+            console.error(err)
         }
     },
     isCmd: true,
