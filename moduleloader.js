e2ff9e5b4534adaa48b7ce94952056b026d66ac6
TTtie <tttiett@outlook.com>
2017-06-06 17:31:52 +0200

    revert pls

--- moduleloader.js
+++ moduleloader.js
@@ -1,18 +1,18 @@
 global.fs = require("fs")
 global.speakerPhoneBinds = require("./speakerphonemodule");
-global.queries  = require("./queries/index")
+global.queries = require("./queries/index")
 global.userQuery = queries.user;
 require("./checkConfig")();
 require("./discord")()
 require("./webserver/index")
 global.db = require("rethinkdbdash")({
-    db: "ttalpha"
+  db: "ttalpha"
 });
 global.keymetrics = require("./keymetrics/index.js");
 global.moment = require("moment");
 global.decimalToHex = function (d) {
   var hex = Number(d).toString(16);
-  hex = "000000".substr(0, 6 - hex.length) + hex; 
+  hex = "000000".substr(0, 6 - hex.length) + hex;
   return hex;
 }
 global.translations = new (require("./translations/index"))()
