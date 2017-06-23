308f7c1a47c0cfe3bb098d53f2a76eab7b720a8a
TTtie <tttiett@outlook.com>
2017-06-23 22:05:45 +0200

    katz-n-dogz

---
+++ commands/dog.js
@@ -0,0 +1,10 @@
+module.exports = {
+    exec: async function (msg, args) {
+         let data = await require("superagent").get("https://random.dog/woof")
+          msg.channel.createMessage({embed:{image:{url: "https://random.dog/"+data.text}, color: 0x008800}})
+    },
+    isCmd: true,
+    display: true,
+    category: 1,
+    description: "Gets a random cat picture.",
+}
