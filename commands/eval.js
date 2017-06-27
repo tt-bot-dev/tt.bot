5a38f4225ab1fef256e0cb81b240105d263cdd72
TTtie <tttiett@outlook.com>
2017-06-27 17:18:12 +0200

    upload a gist if the data is long

--- commands/eval.js
+++ commands/eval.js
@@ -1,24 +1,61 @@
 module.exports = {
-    exec: async function (msg,args) {
+    exec: async function (msg, args) {
         if (isO(msg)) {
             let evaLUAted
-            try {evaLUAted = await eval(`(async () => {${args}})()`)}
-            catch(err) {evaLUAted = err.message; console.error(err.stack)}
+            try { evaLUAted = await eval(`(async () => {${args}})()`) }
+            catch (err) { evaLUAted = err.message; console.error(err.stack) }
             let overall;
             if (typeof evaLUAted !== "string") {
                 overall = require("util").inspect(evaLUAted)
             } else overall = evaLUAted
-            bot.createMessage(msg.channel.id, {embed: {
-                title: "Evaluated!",
-                description: overall.replace(new RegExp(bot.token, "g"), "jako rilý?"),
-                color: 0x008800
-            }})
+            let data = `\`\`\`js\n${overall.replace(new RegExp(bot.token, "g"), "jako rilý?")}\n\`\`\``
+            if (data.length > 2048) {
+                let gist
+                try {
+                    gist = await require("superagent").post("https://api.github.com/gists").send({
+                        description: "Evaluated code",
+                        public: true,
+                        files: {
+                            "exec.md": {
+                                content: data
+                            }
+                        }
+                    })
+                    
+                } catch(err) {
+                    console.log(overall) // we don't replace anything here, because that's console
+                }
+                return await msg.channel.createMessage({
+                    embed: {
+                        title: "Evaluated!",
+                        color: 0x008800,
+                        description: `The data are too long. [View the gist here](${gist.body.html_url})`
+                    }
+                })
+            }
+            bot.createMessage(msg.channel.id, {
+                embed: {
+                    title: "Evaluated!",
+                    description: overall.replace(new RegExp(bot.token, "g"), "jako rilý?"),
+                    color: 0x008800
+                }
+            })
         }
     },
     category: 2,
-    isCmd:true,
+    isCmd: true,
     description: "evaluates js. if ur not an owner go away",
-    display:true,
-    name:"eval",
+    display: true,
+    name: "eval",
     args: "<code>"
 }
+/*
+require("superagent").post("https://api.github.com/gists").send({
+                description: "Evaluated code",
+                public: true,
+                files: {
+                    "exec.md": {
+                        content: data
+                    }
+                }
+            })*/
