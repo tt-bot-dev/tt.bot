a9cbf13330511bee8f2fbb5a54e4ec90cb1c880a
TTtie <tttiett@outlook.com>
2017-07-18 12:19:15 +0200

    make config creation one function

--- commands/config.js
+++ commands/config.js
@@ -1,20 +1,20 @@
 module.exports = {
     exec: async function (msg, args) {
+        async function makeCfg() {
+            await db.table("configs").insert({
+                id: msg.guild.id,
+                modRole: "tt.bot mod",
+                prefix: config.prefix
+            });
+            return await db.table("configs").get(msg.guild.id).run();
+        }
         if (await bot.isModerator(msg.member)) {
             if (args) {
-
                 let c = args.split(" ");
                 let setting = c[0];
                 let value = c.slice(1).join(" ");
                 let server = await db.table("configs").get(msg.guild.id).run();
-                if (!server) {
-                    await db.table("configs").insert({
-                        id: msg.guild.id,
-                        modRole: "tt.bot mod",
-                        prefix: config.prefix
-                    });
-                    server = await db.table("configs").get(msg.guild.id).run();
-                }
+                if (!server) server = await makeCfg();
                 if (setting && value && server[setting]) {
                     server[setting] = value;
                     await db.table("configs").get(msg.guild.id).update(server).run();
@@ -25,14 +25,7 @@ module.exports = {
             }
             else {
                 let server = await db.table("configs").get(msg.guild.id).run();
-                if (!server) {
-                    await db.table("configs").insert({
-                        id: msg.guild.id,
-                        modRole: "tt.bot mod",
-                        prefix: config.prefix
-                    });
-                    server = await db.table("configs").get(msg.guild.id).run();
-                }
+                if (!server) server = await makeCfg();
                 let items = [];
                 Object.keys(server).forEach(item => {
                     if (item != "id") items.push(`${item} - ${server[item]}`);
