efe1f651e5349d00d5313d3f615a7e5101207f98
TTtie <tttiett@outlook.com>
2017-07-19 12:53:28 +0200

    fml

--- evt.js
+++ evt.js
@@ -1,11 +1,10 @@
 let fi = fs.readdirSync("./events");
 fi.forEach(e => {
     if (/.+\.js$/.test(e)) {
-        let cmN = e.match(/(.+)\.js$/)[1];
-        let cmFL = require("./events/" + e  + ".js");
+        let cmFL = require("./events/" + e);
         if (cmFL.isEvent) {
-            console.log(`${__filename}      | Loading ${cmN} event, file ${e}`);
-            bot.on(cmN, cmFL);
+            console.log(`${__filename}      | Loading ${e.replace(/\.js/, "")} event, file ${e}`);
+            bot.on(e.replace(/\.js/, ""), cmFL);
         }
         else console.log(__filename + "    | Skipping non-event " + e);
     } else {
