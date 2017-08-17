2e62b4a142d8a4a069e395196f1f8026c58ff00e
TTtie <tttiett@outlook.com>
2017-08-17 16:05:36 +0200

    fuck discord

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = async function () {
     global.connected = true;
     global.cmdWrap = require("../cmdwrapper");
     cmdWrap.loadAll();
-    bot.editStatus("online", { name: `Type ${config.prefix}help` });
+    bot.editStatus("online", { name: `Type ${config.prefix}help`, type: 0 });
     bot.postStats().then(console.log(__filename + "     | Successfully posted!"), r => console.log(r.body));
     bot.postStats2().then(console.log(__filename + "     | Successfully posted 2!"), r => console.log(r.body));
     global.keymetricsMetrics = new keymetrics();
