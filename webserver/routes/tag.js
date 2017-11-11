fbd4cb48c585ff49b8da40d37acc1496cfa581e5
TTtie <tttiett@outlook.com>
2017-11-11 16:39:48 +0100

    fixes #10

--- webserver/routes/tag.js
+++ webserver/routes/tag.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const app = Router();
+const tagStruct = require("../Structures/TagObject")
 app.get("/", checkOwner, async (req, res) => {
     res.render("tag-list", req.makeTemplatingData({
-        tags: await db.table("tags").run()
+        tags: (await db.table("tags").run()).map(t => new tagStruct(t))
     }));
 });
 app.get("/delete/:id", checkOwner, async (req, res) => {
@@ -15,6 +16,7 @@ app.get("/delete/:id", checkOwner, async (req, res) => {
         await db.table("tags").get(req.params.id).delete();
         res.redirect("/tags/");
     }
+    data = new tagStruct(data)
     res.render("delete", req.makeTemplatingData({
         guild: {
             name: `tag named ${data.id}`
@@ -28,7 +30,7 @@ app.get("/view/:id", checkOwner, async (req, res) => {
         return res.render("404", req.makeTemplatingData());
     }
     res.render("tag-details", req.makeTemplatingData({
-        tag: data
+        tag: new tagStruct(data)
     }));
 });
 
