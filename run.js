dbb47817fc049422a592bc049f02e9014f979b03
TTtie <tttiett@outlook.com>
2017-11-16 22:04:02 +0100

    Clean up

--- run.js
+++ run.js
@@ -1,23 +1,9 @@
-process.stdin.resume();
-process.stdin.setEncoding("utf8");
-process.stdin.on("data", (chu) => {
-    var chunk = chu.toString();
-    if (chunk.startsWith("shutdown")) {
-        process.exit(0);
-    } else if (chunk.startsWith("eval ")) {
-        let slice = chunk.slice("eval ".length);
-        try { console.log(eval(slice)); } catch (err) { console.error(err); }
-    }
-});
 process.on("unhandledRejection", (r) => {
     console.log(`${__filename}      | Unhandled rejection, reason:\n ${require("util").inspect(r)}`);
 });
 process.on("uncaughtException", (err) => {
     console.log(`${__filename}      | Unhandled exception`, err.message);
     console.error(err.stack);
-});
-process.on("SIGINT", () => {
-    console.log("Received a SIGINT...");
 });
 global.p = function () { return; };
 global.cmds = {};
