082d653346a4b9554897c970126219c167e0d036
TTtie <tttiett@outlook.com>
2017-11-19 13:00:40 +0100

    Ability to remove the modlog channel

--- commands/modlog.js
+++ commands/modlog.js
@@ -1,6 +1,14 @@
 module.exports = {
     exec: async function (msg, args) {
         if (!msg.guildConfig) return msg.channel.createMessage(`Please create a config using ${config.prefix}config command before proceeding.`)
+        if (args.toLowerCase() == "remove") {
+            if (!msg.guildConfig.modlogChannel) return msg.channel.createMessage(`You don't have a modlog channel set.`)
+            delete msg.guildConfig.modlogChannel
+            await db.table("configs").get(msg.guild.id).update(msg.guildConfig)
+            msg.channel.createMessage(":ok_hand: Removed the modlog channel")
+            return
+        }
+        
         msg.guildConfig.modlogChannel = msg.channel.id;
         await db.table("configs").get(msg.guild.id).update(msg.guildConfig)
         msg.channel.createMessage(":ok_hand: I'll send modlog messages here.")
@@ -8,5 +16,6 @@ module.exports = {
     isCmd: true,
     display: true,
     category: 4,
-    description: "Sets the current channel as modlog channel."
+    description: "Sets the current channel as modlog channel.",
+    args: "[remove]"
 };
