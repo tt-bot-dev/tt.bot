aafba85453f90f4a7e8f8dd02359fb5f9dc2ede5
TTtie <tttiett@outlook.com>
2017-11-19 13:04:25 +0100

    force run))

--- commands/modlog.js
+++ commands/modlog.js
@@ -4,13 +4,13 @@ module.exports = {
         if (args.toLowerCase() == "remove") {
             if (!msg.guildConfig.modlogChannel) return msg.channel.createMessage(`You don't have a modlog channel set.`)
             delete msg.guildConfig.modlogChannel
-            await db.table("configs").get(msg.guild.id).update(msg.guildConfig)
+            await db.table("configs").get(msg.guild.id).update(msg.guildConfig).run()
             msg.channel.createMessage(":ok_hand: Removed the modlog channel")
             return
         }
         
         msg.guildConfig.modlogChannel = msg.channel.id;
-        await db.table("configs").get(msg.guild.id).update(msg.guildConfig)
+        await db.table("configs").get(msg.guild.id).update(msg.guildConfig).run()
         msg.channel.createMessage(":ok_hand: I'll send modlog messages here.")
     },
     isCmd: true,
