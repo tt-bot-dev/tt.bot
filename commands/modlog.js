37fa171cf943df00ae9114fdc7a7251bb2ab3b0f
TTtie <tttiett@outlook.com>
2017-11-19 13:05:57 +0100

    seems like rethink doesn't update things in its entirety

--- commands/modlog.js
+++ commands/modlog.js
@@ -3,7 +3,7 @@ module.exports = {
         if (!msg.guildConfig) return msg.channel.createMessage(`Please create a config using ${config.prefix}config command before proceeding.`)
         if (args.toLowerCase() == "remove") {
             if (!msg.guildConfig.modlogChannel) return msg.channel.createMessage(`You don't have a modlog channel set.`)
-            delete msg.guildConfig.modlogChannel
+            msg.guildConfig.modlogChannel = undefined
             await db.table("configs").get(msg.guild.id).update(msg.guildConfig).run()
             msg.channel.createMessage(":ok_hand: Removed the modlog channel")
             return
