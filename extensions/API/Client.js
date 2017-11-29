dbee2c0e43e83d8763b8b7c8425c52f169091727
TTtie <tttiett@outlook.com>
2017-11-29 14:11:14 +0100

    eslint

--- extensions/API/Client.js
+++ extensions/API/Client.js
@@ -1,19 +1,23 @@
 let origBot, origGuild;
+// A dummy message object so ESLint doesn't complain
+class Message {}
 class Client {
     constructor(bot, guild) {
         origBot = bot;
-        origGuild = guild
+        origGuild = guild;
     }
 
     waitForMessage(authorId, channelId, timeout, check) {
-        if (!authorId || !channelId) return Promise.reject("Missing author/channel ID")
-        if (!check || typeof check !== "function") check = () => true
+        if (!authorId || !channelId) return Promise.reject("Missing author/channel ID");
+        if (!check || typeof check !== "function") check = () => true;
         let c = msg => {
             if (msg.author.id == authorId && msg.channel.id == channelId
                 && msg.channel.guild && msg.channel.guild.id == origGuild.id
                 && check()) return true;
-                else return false;
-        }
-        origBot.waitForEvent("messageCreate", timeout, c).then((eventReturn) => new Message(eventReturn[0]))
+            else return false;
+        };
+        origBot.waitForEvent("messageCreate", timeout, c)
+            .then((eventReturn) => new Message(eventReturn[0]));
     }
 }
+module.exports = Client;
