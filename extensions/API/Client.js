6a5410070c51d8366c48d0b0929fcbe66d1f8892
TTtie <tttiett@outlook.com>
2017-11-29 14:01:37 +0100

    Bases of the API

---
+++ extensions/API/Client.js
@@ -0,0 +1,19 @@
+let origBot, origGuild;
+class Client {
+    constructor(bot, guild) {
+        origBot = bot;
+        origGuild = guild
+    }
+
+    waitForMessage(authorId, channelId, timeout, check) {
+        if (!authorId || !channelId) return Promise.reject("Missing author/channel ID")
+        if (!check || typeof check !== "function") check = () => true
+        let c = msg => {
+            if (msg.author.id == authorId && msg.channel.id == channelId
+                && msg.channel.guild && msg.channel.guild.id == origGuild.id
+                && check()) return true;
+                else return false;
+        }
+        origBot.waitForEvent("messageCreate", timeout, c).then((eventReturn) => new Message(eventReturn[0]))
+    }
+}
