3a4d160ce38559ad9de37db5d32e468af8de5aae
TTtie <tttiett@outlook.com>
2017-12-09 18:59:52 +0100

    User class

---
+++ extensions/API/User.js
@@ -0,0 +1,31 @@
+let origUser = null;
+const Message = require("./Message")
+class User {
+    constructor(user) {
+        origUser = user;
+        this.avatar = user.avatar;
+        this.avatarURL = user.avatarURL;
+        this.bot = user.bot;
+        this.createdAt = user.createdAt;
+        this.defaultAvatar = user.defaultAvatar;
+        this.defaultAvatarURL = user.defaultAvatarURL;
+        this.discriminator = user.discriminator;
+        this.id = user.id;
+        this.mention = user.mention;
+        this.staticAvatarURL = user.staticAvatarURL;
+        this.username = user.username;
+    }
+
+    dynamicAvatarURL(format, size) {
+        return origUser.dynamicAvatarURL(format, size);
+    }
+
+    createMessage(content, file) {
+        if (this.bot) return Promise.reject({
+            code: 50017,
+            message: "Cannot send messages to this user"
+        });
+        return origUser.getDMChannel().then(dm => dm.createMessage(content, file)).then(m => new Message(m))
+    }
+}
+module.exports = User;
