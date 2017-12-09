32e9ad35f1e454875e7902b867bd3bccba586cf1
TTtie <tttiett@outlook.com>
2017-12-09 19:02:00 +0100

    Implement into Message too

--- extensions/API/Message.js
+++ extensions/API/Message.js
@@ -1,9 +1,10 @@
 let origMsg = null
+const User = require("./User");
 class Message {
     constructor(msg) {
         origMsg = msg;
         this.attachments = msg.attachments;
-        // this.author = msg.author;
+        this.author = new User(msg.author);
         // this.channel = msg.channel;
         // this.channelMentions = msg.channelMentions.map(c => this.guild.channels.get(c));
         this.cleanContent = msg.cleanContent;
@@ -13,7 +14,7 @@ class Message {
         this.id = msg.id;
         // this.member = msg.member;
         this.mentionEveryone = msg.mentionEveryone;
-        // this.mentions = msg.mentions;
+        this.mentions = msg.mentions.map(u => new User(u));
         // this.mentionsMembers = msg.mentions.map(u => this.guild.members.get(u))
         this.pinned = msg.pinned;
         this.reactions = msg.reactions;
