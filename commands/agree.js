e3f60fdb0ff620b98157265b1b072a3c39771519
TTtie <tttiett@outlook.com>
2017-12-11 13:55:52 +0100

    Added agree command + web storm files

---
+++ commands/agree.js
@@ -0,0 +1,13 @@
+module.exports = {
+    exec: async function (msg) {
+        const {agreeChannel, memberRole} = msg.guildConfig;
+        if (!agreeChannel || !memberRole) return;
+        if (msg.channel.id !== agreeChannel) return;
+        if (!msg.guild.roles.get(memberRole)) return;
+        msg.member.addRole(memberRole, "Agreement to server's rules")
+    },
+    isCmd: true,
+    display: true,
+    category: 1,
+    description: "If the server has set agree feature, agrees to the server's rules.",
+};
