6ec73ad2ba428bfb6ca48dd1e5acab0b1c83b7cc
TTtie <tttiett@outlook.com>
2018-03-16 18:23:28 +0100

    tell the master that worker doesnt do any work

--- util/e2pworker.js
+++ util/e2pworker.js
@@ -4,7 +4,7 @@ const WORKER_ID = Number(process.env.WORKER_ID);
 pp.on("generateImage", async ({input}, cb) => {
     pp.send("isWorking", {id: WORKER_ID, working: true});
     const {generated, animated, image} = await e2p(input);
-    pp.send("isWorking", {id: WORKER_ID, working: true});
+    pp.send("isWorking", {id: WORKER_ID, working: false});
     cb({generated, animated, image: image.toString("base64")});
 });
 (async () => {
