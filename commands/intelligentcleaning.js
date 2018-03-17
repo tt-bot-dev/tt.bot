83c7f06494b6eeff1a3cac5a750456dbbe5eaeb5
TTtie <tttiett@outlook.com>
2018-03-17 17:11:23 +0100

    add in an alias for clear and add a sweeping command

---
+++ commands/intelligentcleaning.js
@@ -0,0 +1,16 @@
+module.exports = {
+    exec: function (msg) {
+        const configs = (await db.table("configs")).filter(({id}) => !bot.guilds.get(id));
+        const modlogItems = (await db.table("modlog")).filter(({id}) => !bot.guilds.get(id));
+        configs.forEach(async ({id}) => await db.table("configs").get(id).delete());
+        modlogItems.forEach(async ({id}) => await db.table("modlog").get(id).delete());
+        msg.channel.createMessage(`Sweeping is done! Removed ${configs.length} server configs and ${modlogItems.length} modlogs.`)
+    },
+    isCmd: true,
+    display: true,
+    category: 2,
+    description: "Nukes unneeded data.",
+    aliases: [
+        "sweep"
+    ]
+};
