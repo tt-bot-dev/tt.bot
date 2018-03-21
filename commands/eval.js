35287f973e83e95ac80a2fd1434219fc271c9528
TTtie <tttiett@outlook.com>
2018-03-21 16:24:35 +0100

    GitHub pls

--- commands/eval.js
+++ commands/eval.js
@@ -12,28 +12,39 @@ module.exports = {
         const censor = new CensorBuilder();
         let data = `\`\`\`js\n${overall.replace(censor.build(), "jako rilý?")}\n\`\`\``;
         if (data.length > 2048) {
-            let gist;
-            try {
-                gist = await require("superagent").post("https://api.github.com/gists").send({
-                    description: "Evaluated code",
-                    public: true,
-                    files: {
-                        "exec.md": {
-                            content: data
+            if (config.gistKey) {
+                let gist;
+                try {
+                    gist = await require("superagent").post("https://api.github.com/gists").set("Authorization", `Token ${config.gistKey}`).send({
+                        description: "Evaluated code",
+                        public: true,
+                        files: {
+                            "exec.md": {
+                                content: data
+                            }
                         }
+                    });
+
+                } catch (err) {
+                    console.log(overall); // we don't replace anything here, because that's console
+                }
+                return await msg.channel.createMessage({
+                    embed: {
+                        title: "Evaluated!",
+                        color: 0x008800,
+                        description: `The data are too long. [View the gist here](${gist.body.html_url})`
                     }
                 });
-                    
-            } catch(err) {
-                console.log(overall); // we don't replace anything here, because that's console
+            } else {
+                await msg.channel.createMessage({
+                    embed: {
+                        title: "Evaluated!",
+                        color: 0x008800,
+                        description: `Unfortunately, we can't provide the data here because they're too long and you haven't provided a GitHub Gist API key.\nThereby, the output has been logged in the console.`
+                    }
+                })
+                console.log(overall);
             }
-            return await msg.channel.createMessage({
-                embed: {
-                    title: "Evaluated!",
-                    color: 0x008800,
-                    description: `The data are too long. [View the gist here](${gist.body.html_url})`
-                }
-            });
         }
         bot.createMessage(msg.channel.id, {
             embed: {
