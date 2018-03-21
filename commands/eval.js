04425a8dcb90632dbf585922797e046be86169e8
TTtie <tttiett@outlook.com>
2018-03-21 16:26:17 +0100

    yeah don't

--- commands/eval.js
+++ commands/eval.js
@@ -44,6 +44,7 @@ module.exports = {
                     }
                 })
                 console.log(overall);
+                return;
             }
         }
         bot.createMessage(msg.channel.id, {
