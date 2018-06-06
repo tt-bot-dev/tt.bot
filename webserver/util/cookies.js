b3844dee0c03ef3300b2951f91f6cce8d3c9f290
TTtie <tttiett@outlook.com>
2018-06-06 19:13:25 +0200

    Allow DBL to bypass cookie consent

--- webserver/util/cookies.js
+++ webserver/util/cookies.js
@@ -1,6 +1,7 @@
 module.exports = function checkCookieConsent(rq, rs, nx) {
     if (rq.originalUrl.startsWith("/static")) return nx(); // prevent the data consent from loading on static files
     if (rq.originalUrl.startsWith("/acceptcookie")) return nx();
+    if (rq.originalUrl.startsWith("/api")) return nx(); // Allow DBL to bypass cookie consent
     if (!rq.signedCookies) return rs.render("landing-consent", rq.makeTemplatingData());
     if (rq.signedCookies.dataOk !== "ok") return rs.render("landing-consent", rq.makeTemplatingData());
     return nx();
