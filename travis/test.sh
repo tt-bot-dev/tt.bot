3686645bc62505690630c77a8ceb5e7fd50a063d
TTtie <tttiett@outlook.com>
2018-07-02 19:05:13 +0200

    export this env variable

--- travis/test.sh
+++ travis/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-TRAVIS_ALLOW_FAILURE=true # This is linting for god's sake
+export TRAVIS_ALLOW_FAILURE=true # This is linting for god's sake
 if [[ "$TRAVIS_COMMIT_MESSAGE" == noci-* ]]; then
     echo "\e[31mTest triggered for a no CI commit, pushing to OwO and ignoring."
     exit 0
