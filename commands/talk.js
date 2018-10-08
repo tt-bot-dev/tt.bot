ea9d3dd95ecb32f6c70526ba2707b1606d23fa91
TTtie <tttiett@outlook.com>
2018-10-08 18:53:41 +0200

    fix the cleverbutt

--- commands/talk.js
+++ commands/talk.js
@@ -1,21 +1,21 @@
 const s = require("snekfetch");
 module.exports = {
     exec: async function (msg, args) {
-        let errMessages = {
+        const errMessages = {
             queryTooLong: msg.t("QUERY_TOO_LONG"),
             cantTell: msg.t("CANT_TELL")
         };
         if (args > 255) return msg.channel.createMessage(errMessages.queryTooLong);
         try {
-            let r = await s.get("http://api.program-o.com/v2/chatbot/")
+            const r = await s.get("http://api.program-o.com/v2/chatbot/")
                 .query({
                     bot_id: 6,
                     say: args,
                     convo_id: msg.author.id,
                     format: "json"
                 });
-            r.body = JSON.parse(r.text);
-            let resp = r.body.botsay.replace(/Program-O/g, bot.user.username).replace(/\<br\/\>/g, "\n");
+            const body = JSON.parse(r.raw.toString());
+            const resp = body.botsay.replace(/Program-O/g, bot.user.username).replace(/\<br\/\>/g, "\n");
 
             await msg.channel.createMessage(resp);
         } catch (err) {
