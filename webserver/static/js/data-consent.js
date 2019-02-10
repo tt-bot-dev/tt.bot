13ab72e2bdff1cfa26d2aaa0c1c1756ce8fa9b0f
TTtie <tttiett@outlook.com>
2019-02-10 09:48:30 +0100

    Try this?

--- webserver/static/js/data-consent.js
+++ webserver/static/js/data-consent.js
@@ -1,1 +1,1 @@
-window.onload=function(){var o=document.querySelector("#yes"),n=document.querySelector("#no");o.addEventListener("click",function(){window.location="/acceptcookie?redir="+window.location.pathname}),n.addEventListener("click",function(){window.location="https://youtu.be/DLzxrzFCyOs"})};
+window.onload=function(){var o=document.querySelector("#yes"),n=document.querySelector("#no");o.addEventListener("click",function(){window.location="/acceptcookie"}),n.addEventListener("click",function(){window.location="https://youtu.be/DLzxrzFCyOs"})};
