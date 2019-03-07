8fb86c450e91872b63ccc08ffced6206e5b5475e
TTtie <tttiett@outlook.com>
2019-03-07 15:45:11 +0100

    I watched the pipeline log, and I feel this is the way

--- travis/fixlint.sh
+++ travis/fixlint.sh
@@ -17,6 +17,6 @@ echo "Committing"
 git commit -m "Fix linting issues for $CURRENT_COMMIT"
 
 echo "Pushing"
-git push https://tt-bot:$GIT_ACCESS_TOKEN@github.com/tt-bot-dev/tt.bot $CURRENT_BRANCH >/dev/null 2>&1
-
+#git push https://tt-bot:$GIT_ACCESS_TOKEN@github.com/tt-bot-dev/tt.bot $CURRENT_BRANCH >/dev/null 2>&1
+git -c http.extraheader="Authorization: Basic $GIT_ACCESS_TOKEN" push origin $CURRENT_BRANCH
 exit 0
