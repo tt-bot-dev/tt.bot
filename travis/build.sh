04823ecf371d6349aa92349b72ac127785609079
TTtie <tttiett@outlook.com>
2019-03-07 12:46:45 +0100

    Apparently not even like that

--- travis/build.sh
+++ travis/build.sh
@@ -1,3 +1,4 @@
+echo "Pushing to $TRAVIS_BRANCH..."
 git remote add owo https://$GITLAB_USERNAME:$GITLAB_ACCESS_TOKEN@owo.codes/TTtie/tt.bot.git
-git push owo $TRAVIS_BRANCH #>/dev/null 2>&1
+git push owo HEAD:$TRAVIS_BRANCH #>/dev/null 2>&1
 echo "Successfully pushed to OwO"
