dc5a47835ca9e980e9a5eb8cbeac192365e869f4
TTtie <tttiett@outlook.com>
2019-03-07 12:42:48 +0100

    Enable git output for testing purposes

--- travis/build.sh
+++ travis/build.sh
@@ -1,3 +1,3 @@
 git remote add owo https://$GITLAB_USERNAME:$GITLAB_ACCESS_TOKEN@owo.codes/TTtie/tt.bot.git
-git push owo $TRAVIS_BRANCH >/dev/null 2>&1
+git push owo $TRAVIS_BRANCH #>/dev/null 2>&1
 echo "Successfully pushed to OwO"
