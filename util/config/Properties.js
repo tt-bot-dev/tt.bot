c3999a07653f7e289d77eb0b63c7ab09f9e2b1b7
TTtie <tttiett@outlook.com>
2019-10-13 17:34:11 +0200

    remove askYesNo.js and useless quotes

--- util/config/Properties.js
+++ util/config/Properties.js
@@ -1,58 +1,58 @@
 "use strict";
 const Prop = {
-    "prefix": {
+    prefix: {
         type: "string",
         default: config.prefix,
         translationKey: "CONFIG_PREFIX",
         descriptionTranslationKey: "CONFIG_PREFIX_DESCRIPTION"
     },
-    "modRole": {
+    modRole: {
         type: "string",
         translationKey: "CONFIG_MODROLE",
         descriptionTranslationKey: "CONFIG_MODROLE_DESCRIPTION",
         default: "tt.bot mod"
     },
-    "farewellMessage": {
+    farewellMessage: {
         type: "string",
         translationKey: "CONFIG_FAREWELL",
         descriptionTranslationKey: "CONFIG_FAREWELL_DESCRIPTION"
     },
-    "farewellChannelId": {
+    farewellChannelId: {
         type: "channel",
         translationKey: "CONFIG_FAREWELL_CHANNEL",
         descriptionTranslationKey: "CONFIG_FAREWELL_CHANNEL_DESCRIPTION"
     },
-    "greetingMessage": {
+    greetingMessage: {
         type: "string",
         translationKey: "CONFIG_GREETING",
         descriptionTranslationKey: "CONFIG_GREETING_DESCRIPTION"
     },
-    "greetingChannelId": {
+    greetingChannelId: {
         type: "channel",
         translationKey: "CONFIG_GREETING_CHANNEL",
         descriptionTranslationKey: "CONFIG_GREETING_CHANNEL_DESCRIPTION"
     },
-    "agreeChannel": {
+    agreeChannel: {
         type: "channel",
         translationKey: "CONFIG_AGREE_CHANNEL",
         descriptionTranslationKey: "CONFIG_AGREE_CHANNEL_DESCRIPTION"
     },
-    "memberRole": {
+    memberRole: {
         type: "role",
         translationKey: "CONFIG_MEMBER_ROLE",
         descriptionTranslationKey: "CONFIG_MEMBER_ROLE_DESCRIPTION"
     },
-    "logChannel": {
+    logChannel: {
         type: "channel",
         translationKey: "CONFIG_LOG_CHANNEL",
         descriptionTranslationKey: "CONFIG_LOG_CHANNEL_DESCRIPTION"
     },
-    "logEvents": {
+    logEvents: {
         type: "string",
         translationKey: "CONFIG_LOG_EVENTS",
         descriptionTranslationKey: "CONFIG_LOG_EVENTS_DESCRIPTION"
     },
-    "modlogChannel": {
+    modlogChannel: {
         type: "channel",
         translationKey: "CONFIG_MODLOG_CHANNEL",
         descriptionTranslationKey: "CONFIG_MODLOG_CHANNEL_DESCRIPTION"
