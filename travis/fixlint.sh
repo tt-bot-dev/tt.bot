1ec64957162191b5cc42b205198183677081bf77
TTtie <tttiett@outlook.com>
2019-10-16 17:19:21 +0200

    Comment out useless steps

--- travis/fixlint.sh
+++ travis/fixlint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Checking out the current branch ($CURRENT_BRANCH)...."
-git fetch >/dev/null 2>&1
-git checkout $CURRENT_BRANCH
-git config --global user.name "tt.bot" >/dev/null 2>&1
-# this is on purpose, it's public, feel free to email me! - TTtie 2019
-git config --global user.email \
-    "whatdidyouthinkiwontleakmyprivate@email.cz" \
->/dev/null 2>&1 
+# git fetch >/dev/null 2>&1
+# git checkout $CURRENT_BRANCH
+# git config --global user.name "tt.bot" >/dev/null 2>&1
+# # this is on purpose, it's public, feel free to email me! - TTtie 2019
+# git config --global user.email \
+#     "whatdidyouthinkiwontleakmyprivate@email.cz" \
+# >/dev/null 2>&1 
 
 echo "Linting, fixing the issues"
 npm test -- --fix
