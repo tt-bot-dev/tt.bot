d1f53b8e2a76c301855e4b16af2f116312f74938
TTtie <tttiett@outlook.com>
2020-08-14 00:06:49 +0200

    Show user flags in userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -19,7 +19,7 @@
 
 "use strict";
 const { Command, SerializedArgumentParser,
-    Serializers: { Member } } = require("sosamba");
+    Serializers: { Member }, Eris: {Constants: {UserFlags}} } = require("sosamba");
 const userByID = require("../lib/util/userByID");
 const moment = require("moment");
 const config = require("../config");
@@ -50,6 +50,7 @@ class UserCommand extends Command {
             ctx.send({
                 embed: {
                     title: await ctx.t("USER_INFO", `${nick} ${nick === this.sosamba.getTag(user) ? "" : `(${this.sosamba.getTag(user)})`} (${user.id}) ${user.bot ? "(BOT)" : ""}`),
+                    description: user.user.publicFlags ? this.bitArrayToEmoji(ctx, this.parseUserBitfield(user.user.publicFlags)).join("\n") : undefined,
                     thumbnail: {
                         url: user.user.avatarURL
                     },
@@ -80,6 +81,7 @@ class UserCommand extends Command {
             await ctx.send({
                 embed: {
                     title: await ctx.t("USER_INFO", `${this.sosamba.getTag(user)} ${user.bot ? "(BOT)" : ""}`),
+                    description: user.publicFlags ? this.bitArrayToEmoji(ctx, this.parseUserBitfield(user.publicFlags)).join("\n") : undefined,
                     thumbnail: {
                         url: user.avatarURL
                     },
@@ -98,6 +100,39 @@ class UserCommand extends Command {
             });
         }
         
+    }
+
+    parseUserBitfield(bitfield) {
+        let out = [];
+        for (const k of Object.keys(UserFlags)) {
+            if (k === "NONE") continue;
+            if (bitfield & UserFlags[k]) out.push(k);
+        }
+        return out;
+    }
+
+    bitArrayToEmoji(ctx, bitArray) {
+        let out = [];
+        const canUseExternal = this.sosamba.hasBotPermission(ctx.channel, "externalEmojis");
+
+        // Emojis taken from discord.bots.gg Discord guild
+        if (bitArray.includes("DISCORD_EMPLOYEE")) out.push(`${canUseExternal ? "<:e:314348604095594498>" : ":tools:"} This user is a Discord employee`);
+        if (bitArray.includes("DISCORD_PARTNER")) out.push(`${canUseExternal ? "<:e:314068430556758017>": ":infinity:"} This user is partnered with Discord`);
+        if (bitArray.includes("HYPESQUAD_EVENTS")) out.push(`${canUseExternal ? "<:e:585765895939424258>" : ":sparkles:"} This user is a HypeSquad event attendee/coordinator`);
+        if (bitArray.includes("BUG_HUNTER_LEVEL_1")) out.push(`${canUseExternal ? "<:e:585765206769139723>" : ":bug:"} This user is a Discord Bug Hunter (level 1)`);
+        if (bitArray.includes("HOUSE_BRAVERY")) out.push(`${canUseExternal ? "<:e:585763004218343426>" : ":shield:"} This user is in the House of Bravery`);
+        if (bitArray.includes("HOUSE_BRILLIANCE")) out.push(`${canUseExternal ? "<:e:585763004495298575>" : ":sunglasses:"} This user is in the House of Brilliance`);
+        if (bitArray.includes("HOUSE_BALANCE")) out.push(`${canUseExternal ? "<:e:585763004574859273>" : ":scales:"} This user is in the House of Balance`);
+        if (bitArray.includes("EARLY_SUPPORTER")) out.push(`${canUseExternal ? "<:e:585763690868113455>" : ":medal:"} This user is an early supporter of Discord`);
+        if (bitArray.includes("TEAM_USER")) out.push(":busts_in_silhouette: This user is tied to a [team](https://discord.com/developers/docs/topics/teams)");
+        if (bitArray.includes("SYSTEM")) out.push(":robot: This user is a Discord system user");
+
+        //Emojis taken from our internal guild
+        if (bitArray.includes("BUG_HUNTER_LEVEL_2")) out.push(`${canUseExternal ? "<:e:743588190241292449>" : ":bug:"} This user is a Discord Bug Hunter (level 2)`);
+        if (bitArray.includes("VERIFIED_BOT")) out.push(`:white_check_mark: This bot is verified`);
+        if (bitArray.includes("VERIFIED_BOT_DEVELOPER")) out.push(`${canUseExternal ? "<:e:743589119799590955>" : ":white_check_mark"} This user is a verified bot developer`);
+
+        return out;
     }
 }
 
